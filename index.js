e599102482e1750c496cebb368122c88310cff1c
nethanjan <nethanjanan@gmail.com>
2020-04-21 20:04:58 +0530

    master: test commit

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const app = express();
 app.get('/', (req, res) => {
     res.send({ hi: 'there'});
 });
-
+console.log('test');
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {console.log('App is listening to http://localhost:5000')});
